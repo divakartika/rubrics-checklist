1458ae2f92f71773944f459bdc3bf451cdf2cfb5
Diva Kartika <divakartika.larasati@gmail.com>
2023-07-29 23:00:35 +0700

    feat: modify Welcome.py
    - Add page configuration (page title, page icon)
    - Put README into Welcome.py (to reduce copy-pasting)

--- Welcome.py
+++ Welcome.py
@@ -3,4 +3,17 @@ import streamlit as st
 
 locale.setlocale(locale.LC_ALL, 'id_ID.UTF-8')
 
-st.title("Welcome to Rubrics Checklist Page! ✅")
+# page configuration
+st.set_page_config(
+    page_title="Rubrics Checklist",
+    page_icon="https://algorit.ma/wp-content/uploads/2023/04/Algoritma-Logo.png",
+)
+
+# get text from README.md
+with open('README.md', mode='r', encoding='utf-8') as f:
+    content = f.read()
+
+content
+
+# close the file
+f.close()
