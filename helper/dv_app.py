bb1d9f13da47318a795a894be11da5639625bc88
Diva Kartika <divakartika.larasati@gmail.com>
2023-10-01 00:39:48 +0700

    feat: add dv capstone

---
+++ helper/dv_app.py
@@ -0,0 +1,84 @@
+import streamlit as st
+from helper import dv_functions as func
+
+def app(case, caps):
+    caps_final, topic_dict, list_df = func.data_prep(caps)
+
+    # initiate empty list & counter
+    list_editor = []
+    count = 0
+
+    # title & header
+    st.title(f"Case: {case}")
+    st.header('Rubrics Checklist')
+
+    # configure reset button
+    if 'button' not in st.session_state:
+        st.session_state.button = False
+
+    def click_button():
+        st.session_state.button = not st.session_state.button
+
+    st.sidebar.write('Click this button to Reset!')
+    st.sidebar.button('Reset', on_click=click_button)
+
+    # sequentially shows: Topic, Category, and dataframes (small dataframes made above)
+    for topic in topic_dict.index:
+        st.subheader(topic)
+        cat_length = len(topic_dict.loc[topic, 'Category'])
+        for num in range(cat_length):
+            st.write(f"**{topic_dict.loc[topic, 'Category'][num]}**") # bold text
+            st.write(topic_dict.loc[topic, 'Explanation'][num])
+            try:
+                plot = topic_dict.loc[topic, 'Plot'][num].split(',')
+                caption = topic_dict.loc[topic, 'Caption'][num]
+                st.image(plot, caption=caption.split(',') if isinstance(caption, str) else caption)
+            except:
+                pass
+
+            #convert dataframe to data_editor
+            if st.session_state.button:
+                caps_editor = st.data_editor(list_df[count], disabled=['Subcategory'], key=f'df_{count}{count}', hide_index=True)
+            else:
+                caps_editor = st.data_editor(list_df[count], disabled=['Subcategory'], key=f'df_{count}', hide_index=True)
+                
+            # save all data_editor to list_editor
+            list_editor.append(caps_editor)
+            # increase count to continue for loop of list_df
+            count += 1
+
+    # change the content of caps_final based on the data_editors
+    for i in range(len(list_editor)):
+        if list_editor[i]['Checks'].all():
+            caps_final.loc[i, 'Checks'] = True
+            caps_final.loc[i, 'Earned'] = caps_final.loc[i, 'Max Point']
+
+    # total points & total earned
+    total_points = int(caps_final['Max Point'].sum())
+    total_earned = int(caps_final['Earned'].sum())
+
+    st.divider()
+    st.header(f"Final Score: {total_earned}/{total_points}")
+
+    # shows final dataframe
+    st.dataframe(caps_final)
+
+    st.divider()
+    st.header("Feedback")
+
+    col1, col2, col3 = st.columns(3)
+    student = col1.text_input('Input Student Name', placeholder='Student Name')
+    honorific = col2.selectbox('Choose Honorific', ['Bapak', 'Ibu', 'Mas', 'Mbak', 'Kak'])
+    is_late = col3.selectbox('Is Late?', ['No', '1 Day', '2 Day', '3 Day', '4 Day', '> 4 Day'])
+
+    # generate feedback
+    feedback = func.generate_text(case, student, honorific, topic_dict, caps_final, total_earned, is_late)
+    st.write(feedback)
+
+    # warning box
+    st.warning(""":warning: WARNING
+               
+               \nTo paste the text in Google Classroom, use **`CTRL+SHIFT+V`**
+               
+               \nUnless you want to make your text looks like a mess, don't use `CTRL+V` 
+               because this Streamlit uses Markdown formatting and Google Classroom uses Rich Text Formatting""")
---
+++ helper/dv_functions.py
@@ -0,0 +1,116 @@
+import locale
+import math
+import numpy as np
+from string import Template
+from datetime import datetime
+from datetime import timedelta
+
+locale.setlocale(locale.LC_ALL, 'id_ID.UTF-8')
+
+def data_prep(caps):
+    # data wrangling
+    caps_clean = caps.copy()
+    caps_clean.iloc[:,:-2] = caps_clean.iloc[:,:-2].ffill()
+    caps_clean.iloc[:,-2:] = caps_clean.iloc[:,-2:].replace(np.nan, None)
+    caps_topic = caps_clean[['Topic', 'Category', 'Explanation', 'Plot', 'Caption']].drop_duplicates()
+    caps_final = caps_clean.groupby(['Category'], sort=False).max(numeric_only = True).reset_index()
+
+    # make a list of unique values of Topic & Category
+    category = caps_clean['Category'].unique()
+    # make a dictionary of Topic containing Category
+    topic_dict = caps_topic.groupby(['Topic'], sort=False).agg(lambda x: list(x))
+
+    list_df = []
+    # split caps_clean into smaller dataframes for each Category, and save the dataframes to list_df
+    for cat in category:
+        df = caps_clean.loc[caps_clean['Category'] == cat, ['Subcategory', 'Checks']]
+        list_df.append(df)
+    
+    return caps_final, topic_dict, list_df
+
+def count_minus_score(is_late):
+    minus_score = {'No': False,
+                  '1 Day': 1,
+                  '2 Day': 3,
+                  '3 Day': 5,
+                  '4 Day': 7,
+                  '> 4 Day': 9
+                  }
+    # return minus score with late day in bahasa ("2 day" is not grammatically correct tho, it is on purpose for the sake of simplicity)
+    return minus_score[is_late], is_late.replace('Day', 'hari')
+
+# check if score >= minimum_points
+def final_txt(total_earned, is_late, caps_final):
+    # get minus skor and late day in bahasa, just changing the day -> hari :(
+    minus_score, late_day = count_minus_score(is_late)
+    total = total_earned - minus_score
+
+    # get total points from data_prep function
+    total_points = int(caps_final['Max Point'].sum())
+    minimum_points = math.floor(0.8*total_points)
+
+    days = 3 if total_points == 16 else 7
+
+    # template text
+    template = {'sukses': "Kami ucapkan selamat atas keberhasilannya dalam mengaplikasikan pembelajaran di kelas terhadap real-world data.",
+                'terlambat': f"Dengan keterlambatan pengumpulan {late_day} (-{minus_score} poin), skor akhir yang Anda dapatkan adalah {total}/{total_points}.",
+                'revisi': f"Karena skor rubrik < {minimum_points}, Anda diperbolehkan untuk merevisi capstone project.",
+                'closing_sukses': "Keep up your good work!",
+                'closing_terlambat': f" dikurangi penalti keterlambatan ({minimum_points} - {minus_score} = {minimum_points-minus_score} poin)",
+                'closing_revisi': f"Maksimal skor setelah revisi adalah {minimum_points}%s. Silakan mengirimkan revisi hingga hari %s, pukul 23.59 WIB. Kami tunggu hasil revisinya!"}
+    # if >= minimum_points and not late
+    if total_earned >= minimum_points:
+        closing = template['closing_sukses']
+        text = f"{template['sukses']} {closing}"
+        # if >= minimum_points and late
+        if minus_score:
+            # generate terlambat template and concat with "">= minimum_points and not late" text
+            text = f"{template['terlambat']}\n\n{text}"
+    # if < minimum_points
+    else:
+        dt = datetime.now() + timedelta(days)
+        deadline = dt.strftime("%A, %d %B %Y").replace(" 0", " ")
+        # generate revisi text from tempalte
+        closing = template['closing_revisi']
+        text = f"{template['revisi']} {closing  % ('', deadline)}"
+        # if < minimum_points and late
+        if minus_score:
+            closing_terlambat = template['closing_terlambat']
+            # generate from terlambat template and concat with revisi text
+            text = f"{template['revisi']}\n\n{template['terlambat']} {closing % (closing_terlambat, deadline)}"
+    
+    return text, total_points
+    
+def generate_text(case, student, honorific, topic_dict, caps_final, total_earned, is_late):
+    text_output = ""
+    count = 0
+
+    for topic in topic_dict.index:
+        text_output += topic + '  \n'
+        for value in topic_dict.loc[topic, 'Category']:
+            earned = int(caps_final.loc[count, 'Earned'])
+            max_point = int(caps_final.loc[count, 'Max Point'])
+            icon = '✅' if earned == max_point else '❌'
+
+            text_output += f"{icon} [{earned}/{max_point}] {value}  \n"
+            count +=1
+        text_output += '\n\n'
+
+    # read text from template.txt
+    with open('helper/template.txt', mode='r', encoding='utf-8') as f:
+        content = f.read()
+        temp = Template(content)
+        # determine the final text based on score
+        final_text, total_points = final_txt(total_earned, is_late, caps_final)
+        # Subtitute
+        feedback = temp.substitute(
+            HONORIFIC = honorific,
+            NAME = student,
+            CASE = case,
+            POINTS = text_output,
+            EARNED = total_earned,
+            TOTAL_POINTS = total_points,
+            FINAL_TEXT = final_text
+        )
+    
+    return feedback
